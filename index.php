d256294181ac1d08b3d4bc6683a35941f93f25b0
prashon <prashon109939@gmail.com>
2024-02-05 09:48:18 +0545

    js added

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-    
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+
+<body>
+    <blockquote>
+        <h1>Students Record</h1>
+        <hr>
+        <table border="1" width="100%">
+            <thead>
+                <tr>
+                    <th>S.No</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody id="students_list"></tbody>
+        </table>
+    </blockquote>
+    <script src="./js/custom.js"></script>
+</body>
+
+</html>
