b8ef03515d3176fff54626ca4a3e30a1b57ab35e
prashon <prashon109939@gmail.com>
2024-02-05 09:58:34 +0545

    loop in script

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,29 @@
-"use strict"
+"use strict";
 
-function getStudents() { 
-    fetch('http://localhost/ajaxCRUD/api.php').then((res)=>{
-        return res.json();
-    }).then((data)=>{
-        console.log(data);
-    }).catch((err)=>{
-        console.log(err);
+function getStudents() {
+  fetch("http://localhost/ajaxCRUD/api.php")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let output = "";
+      data.map((student, index) => {
+        output += `<tr>
+            <td>${++index}</td>
+            <td>${student.name}</td>
+            <td>${student.email}</td>
+            <td>${student.address}</td>
+            <td>
+                <button>Edit</button>
+                <button>Delete</button>
+            </td>
+            </tr>`;
+      });
+      document.getElementById("students_list").innerHTML = output;
+    })
+    .catch((err) => {
+      console.log(err);
     });
- }
+}
 
- getStudents();
+getStudents();
